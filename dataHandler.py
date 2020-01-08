2c567d489ddc2c62a506542c98c5ad9bf87165c3
str1xhyper <unconfigured@null.spigotmc.org>
2020-01-08 19:01:12 +0100

    DataHandler

--- dataHandler.py
+++ dataHandler.py
@@ -3,40 +3,133 @@ import shutil
 import time
 import os
 
-constCactus = '(SkyblockCompetitive) (me » itepastra) Cactus'
-constAvocado = '(SkyblockCompetitive) (me » itepastra) Avocado'
-constSewerfruit = '(SkyblockCompetitive) (me » itepastra) Sewerfruit'
-constNetherfruit = '(SkyblockCompetitive) (me » itepastra) Netherfruit'
-constHamburger = '(SkyblockCompetitive) (me » itepastra) Hamburger'
-constSeafruit = '(SkyblockCompetitive) (me » itepastra) Seafruit'
-constLemon = '(SkyblockCompetitive) (me » itepastra) Lemon'
-constCoconut = '(SkyblockCompetitive) (me » itepastra) Cocunut'
-constPear = '(SkyblockCompetitive) (me » itepastra) Pear'
-constEggplant = '(SkyblockCompetitive) (me » itepastra) Eggplant'
-constApple = '(SkyblockCompetitive) (me » itepastra) Apple'
-constBeetroot = '(SkyblockCompetitive) (me » itepastra) Beetroot'
-constBubblegum = '(SkyblockCompetitive) (me » itepastra) Bubblegum'
-constSlimereed = '(SkyblockCompetitive) (me » itepastra) Slimereed'
-constBlazereed = '(SkyblockCompetitive) (me » itepastra) Blazereed'
-constRubybush = '(SkyblockCompetitive) (me » itepastra) Rubybush'
-constCherry = '(SkyblockCompetitive) (me » itepastra) Cherry'
-constTaco = '(SkyblockCompetitive) (me » itepastra) Taco'
-constChocolate = '(SkyblockCompetitive) (me » itepastra) Chocolate'
-constEnderbloom = '(SkyblockCompetitive) (me » itepastra) Enderbloom'
-constGoldenapple = '(SkyblockCompetitive) (me » itepastra) Goldenapple'
-constPumpkinpie = '(SkyblockCompetitive) (me » itepastra) Pumpkin pie'
-constFootball = '(SkyblockCompetitive) (me » itepastra) FootBall'
-constTurkeyTrap = '(SkyblockCompetitive) (me » itepastra) TurkeyTrap'
-constIceBall = '(SkyblockCompetitive) (me » itepastra) IceBall'
+constPlants = {
+    'Cactus',
+    'Avocado',
+    'Sewerfruit',
+    'Netherfruit',
+    'Seafruit',
+    'Lemon',
+    'Coconut',
+    'Pear',
+    'Eggplant',
+    'Apple',
+    'Beetroot',
+    'Bubblegum',
+    'Slimereed',
+    'Blazereed',
+    'Rubybush',
+    'Cherry',
+    'Taco',
+    'Chocolate',
+    'Enderbloom',
+    'Goldenapple',
+    'PumpkinPie',
+    'FootBall',
+    'TurkeyTrap',
+    'IceBall'
+}
 
-def filepath(
-    filename
-):  # deze functie geeft het path van het bestand zelf terug hoe je het ook uitvoert, zodat het programma het bestand goed kan vinden
+Cactus = []
+Avocado = []
+Sewerfruit = []
+Netherfruit = []
+Seafruit = []
+Lemon = []
+Coconut = []
+Pear = []
+Eggplant = []
+Apple = []
+Beetroot = []
+Bubblegum = []
+Slimereed = []
+Blazereed = []
+Rubybush = []
+Cherry = []
+Taco = []
+Chocolate = []
+Enderbloom = []
+Goldenapple = []
+PumpkinPie = []
+Football = []
+TurkeyTrap = []
+IceBall = []
+
+def filepath(filename):  # deze functie geeft het path van het bestand zelf terug hoe je het ook uitvoert, zodat het programma het bestand goed kan vinden
     script_dir = os.path.dirname(__file__)
     return os.path.join(script_dir, filename)
 
+def AddMoneyToSeedArray(Seed, Money):
+    if Seed == "Cactus":
+        Cactus.append(Money)
+    elif Seed == "Avocado":
+        Avocado.append(Money)
+    elif Seed == "Sewerfruit":
+        Sewerfruit.append(Money)
+    elif Seed == "Netherfruit":
+        Netherfruit.append(Money)
+    elif Seed == "Seafruit":
+        Seafruit.append(Money)
+    elif Seed == "Lemon":
+        Lemon.append(Money)
+    elif Seed == "Coconut":
+        Coconut.append(Money)
+    elif Seed == "Pear":
+        Pear.append(Money)
+    elif Seed == "Eggplant":
+        Eggplant.append(Money)
+    elif Seed == "Apple":
+        Apple.append(Money)
+    elif Seed == "Beetroot":
+        Beetroot.append(Money)
+    elif Seed == "Bubblegum":
+        Bubblegum.append(Money)
+    elif Seed == "Slimereed":
+        Slimereed.append(Money)
+    elif Seed == "Blazereed":
+        Blazereed.append(Money)
+    elif Seed == "Rubybush":
+        Rubybush.append(Money)
+    elif Seed == "Cherry":
+        Cherry.append(Money)
+    elif Seed == "Taco":
+        Taco.append(Money)
+    elif Seed == "Chocolate":
+        Chocolate.append(Money)
+    elif Seed == "Enderbloom":
+        Enderbloom.append(Money)
+    elif Seed == "Goldenapple":
+        Goldenapple.append(Money)
+    elif Seed == "PumpkinPie":
+        PumpkinPie.append(Money)
+    elif Seed == "Football":
+        Football.append(Money)
+    elif Seed == "TurkeyTrap":
+        TurkeyTrap.append(Money)
+    elif Seed == "IceBall":
+        IceBall.append(Money)
+
 with open(filepath('gegevens.txt'), "r") as file:
     data = file.readlines()
+    harvestedSeed = ""
     for line in data:
-        splitLine = line.split(" ")
-        print(splitLine[4])
+        if line != "\n":
+            splitLine = line.split(" ")
+            if(splitLine[3].startswith("(Sky")):
+                for Seed in constPlants:
+                    if splitLine[7].replace("\n", "").lower() == Seed.lower():
+                        harvestedSeed = Seed
+                        print(Seed + " has been started to be harvested")
+                    elif splitLine[7].replace("\n", "").lower() == "stop":
+                        harvestedSeed = ""
+                        print("Last seed has been stopped harvesting")
+            elif(splitLine[5].startswith("$")):
+                noDollar = splitLine[5].split("$")[1]
+                if(noDollar.endswith("B")):
+                    AddMoneyToSeedArray(harvestedSeed, int(float(noDollar.split("B")[0]) * 1000000000))
+                elif(noDollar.endswith("M")):
+                    AddMoneyToSeedArray(harvestedSeed, int(float(noDollar.split("M")[0]) * 1000000))
+                else:
+                    AddMoneyToSeedArray(harvestedSeed, int(float(noDollar.replace(",", ""))))
+print(Apple)
+print(IceBall)
